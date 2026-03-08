271ba765af33e5d97bb5bf5835e8aaa44056c055
Aditya Gaonkar <142329059+AdiGaonkar@users.noreply.github.com>
2026-03-08 20:18:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,12 @@ import addressRouter from "./route/address.route.js";
 import orderRouter from "./route/order.route.js";
 
 const app = express()
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://e-commerce-frontend-ruddy-beta.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
@@ -28,32 +32,30 @@ app.use(cors({
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(morgan())
+app.use(morgan("dev"))
 app.use(helmet({
-    crossOriginResourcePolicy : false
+  crossOriginResourcePolicy: false
 }))
 
-const PORT = 8080 || process.env.PORT 
+const PORT = process.env.PORT || 8080
 
-app.get("/",(request,response)=>{
-    ///server to client
-    response.json({
-        message : "Server is running " + PORT
-    })
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running on port " + PORT
+  })
 })
 
-app.use('/api/user',userRouter)
-app.use("/api/category",categoryRouter)
-app.use("/api/file",uploadRouter)
-app.use("/api/subcategory",subCategoryRouter)
-app.use("/api/product",productRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/address",addressRouter)
-app.use('/api/order',orderRouter)
+app.use('/api/user', userRouter)
+app.use("/api/category", categoryRouter)
+app.use("/api/file", uploadRouter)
+app.use("/api/subcategory", subCategoryRouter)
+app.use("/api/product", productRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/address", addressRouter)
+app.use('/api/order', orderRouter)
 
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("Server is running",PORT)
-    })
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server running on", PORT)
+  })
 })
-
